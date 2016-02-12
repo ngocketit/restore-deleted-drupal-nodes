c96e1484b743b01345a864641725a2687fac5c6e
Phi Van Ngoc <phi.vanngoc@activearkjwt.com>
2016-02-12 11:19:23 +0200

    Make the script more generic

--- restore.rb
+++ restore.rb
@@ -8,46 +8,145 @@ require 'csv'
 require 'optparse'
 
 $cli = HighLine.new
-$docroot = '/srv/git/customers/konecranes/fluid/konecranes/docroot'
-$def_src_nids = "887,955,956,957,958,959,960,961,962,964,969,107961,117266,119256,119471,119481,119486,119491,120206,123981,124141"
-$exc_langs = "'en', 'en-US'"
+$local_docroot = nil # Put here local docroot if you don't want to be prompted
+$def_src_nids = nil # Put here default source node ids separated by commas
 
 $options = {
+  :site_name => nil,
   :verbose => false,
   :src_nids => $def_src_nids,
-  :env => nil,
+  :src_env => nil,
+  :target_env => nil,
   :limit => -1,
   :langs => nil,
 }
 
 def ask(prompt)
-  $cli.ask prompt.color(:yellow)
+  $cli.ask prompt.color(:yellow) do |q|
+    yield q if block_given?
+  end
+end
+
+def info(message)
+  puts message.color(:green)
 end
 
 def error(message)
   puts message.color(:red)
 end
 
-def local_drush(cmd)
-  Dir.chdir $docroot
-  debug "drush", "drush #{cmd}"
-  `drush #{cmd}`
+def fatal_error(message)
+  error message
+  exit
 end
 
-def remote_drush(cmd)
-  debug "drush", "drush @konecranes.#{$options[:env]} #{cmd}"
-  `drush @konecranes.#{$options[:env]} #{cmd}`
-end
-
-def select_env
-  $options[:env] = $cli.choose do |menu|
-    menu.prompt = "Select the environment to import the nodes to: "
-    menu.choices(:dev, :test, :prod)
+def drush(env, cmd)
+  drush_alias = ''
+  if $options[env] == :local
+    Dir.chdir $local_docroot
+  else
+    drush_alias = "@#{$options[:site_name]}.#{$options[env]}"
   end
+  debug "drush", "drush #{drush_alias} #{cmd}"
+  `drush #{drush_alias} #{cmd}`
+end
+
+def source_drush(cmd)
+  drush :src_env, cmd
+end
+
+def target_drush(cmd)
+  drush :target_env, cmd
+end
+
+def module_exists?(env, module_name)
+  output = drush env, "pm-info #{module_name}"
+  exists = false
+  if matches = output.match(/Status\s*:\s*(enabled|disabled|not installed)\s*/)
+    exists = matches[1] == 'enabled'
+  end
+  exists
+end
+
+def select_site_name
+  drush_folder = File.expand_path('~/.drush')
+  if not File.directory? drush_folder
+    error "Drush folder #{drush_folder} not exist. Abort!"
+    exit
+  end
+
+  aliases = []
+  Dir[File.join(drush_folder, '*.aliases.drushrc.php')].each do |drush_file|
+    aliases << File.basename(drush_file).gsub('.aliases.drushrc.php', '')
+  end
+
+  if aliases.empty?
+    error "There is no Acquia Drush aliases on your system. Please download them from Acquia Cloud and put them into #{drush_folder}"
+    exit
+  end
+
+  $options[:site_name] = $cli.choose do |menu|
+    menu.prompt = "Select the site name: ".color(:yellow)
+    menu.choices(*aliases)
+  end
+end
+
+def select_env(prompt)
+  $cli.choose do |menu|
+    menu.prompt = prompt.color(:yellow)
+    menu.choices(:local, :dev, :test, :prod)
+  end
+end
+
+def select_src_env
+  $options[:src_env] = select_env "Select the source environment from which to import the missing nodes: "
+end
+
+def select_target_env
+  $options[:target_env] = select_env "Select the target environment where missing nodes are imported to: "
+end
+
+def prompt_source_nids
+  $options[:src_nids] = ask "Enter the nid of the source nodes separated by commans: " do |q|
+    q.validate = /[0-9, ]+/
+  end
+end
+
+def prompt_langs
+  $options[:langs] = ask "Enter the language codes of the nodes you want to import (press Enter to skip and ignore language): "
 end
 
 def debug(type, message)
   puts "#{type.upcase.color(:yellow)}: #{message}" if $options[:verbose]
+end
+
+def check_environments
+  if $options[:src_env] == $options[:target_env]
+    fatal_error "Source and target environment can't be the same. Abort!"
+  end
+
+  if $options[:src_env] == :local or $options[:target_env] == :local
+    $local_docroot = ask "What is local docroot? "
+    if not File.directory?($local_docroot)
+      fatal_error "Local docroot #{$local_docroot} not exists. Abort"
+    end
+  end
+end
+
+def check_requirements
+  info "Checking requirements on source & target environments"
+
+  if not module_exists? :src_env, 'node_export'
+    fatal_error "node_export module needs to be installed on source environment. Abort!"
+  else
+    info "Source enviroment: OK"
+  end
+
+  if not module_exists? :target_env, 'node_export'
+    fatal_error "node_export module needs to be installed on target environment. Abort!"
+  else
+    info "Target enviroment: OK"
+  end
 end
 
 def get_missing_nodes
@@ -69,28 +168,28 @@ def get_missing_nodes
   end
 
   query = "SELECT group_concat(nid order by nid asc), count(*) AS count, language, tnid FROM node "\
-    " WHERE status=1 AND tnid IN (#{$options[:src_nids]}) AND nid != tnid AND language NOT IN (#{$exc_langs}) #{types_clause} #{langs_clause}"\
+    " WHERE status=1 AND tnid IN (#{$options[:src_nids]}) AND nid != tnid #{types_clause} #{langs_clause}"\
     " GROUP BY tnid, language HAVING count >= 1 ORDER BY tnid"
 
-  retval = local_drush "sql-query \"#{query}\""
+  retval = source_drush "sql-query \"#{query}\""
   nids = []
-  info = {}
+  node_info = {}
 
   retval.split("\n").each do |line|
     parts = line.split "\t"
     nids << parts[0]
-    info[parts[0]] = parts
+    node_info[parts[0]] = parts
   end
 
-  puts "Found #{nids.size} translated nodes".color(:yellow)
+  info "Found #{nids.size} translated nodes"
 
   missing_nids = {}
-  existing_nids = remote_drush("sql-query \"select nid from node where nid in (#{nids.join(',')})\"").split("\n")
+  existing_nids = target_drush("sql-query \"select nid from node where nid in (#{nids.join(',')})\"").split("\n")
   existing_nids.each do |nid|
-    info.delete(nid)
+    node_info.delete(nid)
   end
-  puts "There are #{info.size} missing nodes".color(:yellow)
-  info
+  info "Found #{node_info.size} missing nodes"
+  node_info
 end
 
 def restore_nodes
@@ -108,32 +207,32 @@ def restore_nodes
       count += 1
     end
 
-    export = local_drush "node-export-export #{nid} --format=json"
+    export = source_drush "node-export-export #{nid} --format=json"
     export = JSON.parse export
     # Reset the tnid to make it a saparate node
     export[0]["tnid"] = 0
     import = JSON.generate export
 
-    puts "Importing node: #{nid}"
+    info "Importing node: #{nid}"
     debug "import",  import
 
     import_file = Tempfile.new("node_import")
     import_file << import
     import_file.close
 
-    retval = `cat #{import_file.path} | drush @konecranes.#{$options[:env]} node-export-import`
+    retval = `cat #{import_file.path} | drush @#{$options[:site_name]}.#{$options[:target_env]} node-export-import`
 
     match = /^Imported node ([0-9]+):(.+)$/.match(retval) do |m|
       # Get old node menu info
-      row = local_drush "sql-query \"select mlid, menu_name, weight, concat_ws(',', p1, p2, p3, p4, p5) from menu_links where link_path='node/#{nid}' limit 1\""
+      row = source_drush "sql-query \"select mlid, menu_name, weight, concat_ws(',', p1, p2, p3, p4, p5) from menu_links where link_path='node/#{nid}' limit 1\""
       cols = row.gsub!("\n", "").split("\t")
 
-      link_titles = local_drush "sql-query \"select group_concat(link_title separator ' --> ') from menu_links where mlid in (#{cols[3]})\""
+      link_titles = source_drush "sql-query \"select group_concat(link_title separator ' --> ') from menu_links where mlid in (#{cols[3]})\""
       link_titles.gsub!("\n", "")
 
       # Find the domain for this node if not existing
       if not domains[info[2]]
-        domain = remote_drush "sql-query \"select domain from languages where language='#{info[2]}'\""
+        domain = target_drush "sql-query \"select domain from languages where language='#{info[2]}'\""
         domains[info[2]] = domain.gsub("\n", "") if not domain.empty?
       end
 
@@ -183,10 +282,10 @@ def restore_nodes
 
       item_info = menu_links_info[menu_name][menu_plids]
 
-      puts "Creating menu link: #{menu_name} -> #{item_info[:menu_link]}: node/#{item_info[:new_nid]}"
+      info "Creating menu link: #{menu_name} -> #{item_info[:menu_link]}: node/#{item_info[:new_nid]}"
 
       # Now check if the closest parent mlid exists
-      last_plid = remote_drush "sql-query \"select mlid from menu_links where menu_name='#{menu_name}' and mlid='#{parent_mlids[-2]}'\""
+      last_plid = target_drush "sql-query \"select mlid from menu_links where menu_name='#{menu_name}' and mlid='#{parent_mlids[-2]}'\""
       last_plid = last_plid.gsub("\n", "")
 
       # We have all parent mlids, create the menu in hidden state
@@ -194,7 +293,7 @@ def restore_nodes
         menu_item_array = "array(\"link_path\" => \"node/#{item_info[:new_nid]}\", \"link_title\" => \"#{item_info[:title]}\",\"weight\" => \"#{item_info[:menu_weight]}\","\
         "\"menu_name\" => \"#{menu_name}\", \"mlid\" => \"#{item_info[:old_mlid]}\", \"plid\" => \"#{last_plid}\", \"hidden\" => \"0\")"
 
-        saved_mlid = remote_drush "eval '$item = #{menu_item_array}; echo menu_link_save($item);'"
+        saved_mlid = target_drush "eval '$item = #{menu_item_array}; echo menu_link_save($item);'"
 
         if saved_mlid.to_i > 0 
           menu_links_info[menu_name][menu_plids][:new_mlid] = saved_mlid
@@ -223,27 +322,35 @@ def restore_nodes
     end
   end
 
-  puts "Successfully imported #{imported_nodes} nodes. Result saved to /tmp/result.csv"
+  info "Restored #{imported_nodes} nodes. Result saved to /tmp/result.csv"
 end
 
 opt_parser = OptionParser.new do |opts|
-  opts.on("-e", "--environment ENV", "Target environment to import to") do |env|
-    $options[:env] = env
+  opts.on("-s", "--sitename SITE", "Name of Acquia Cloud site") do |site|
+    $options[:site_name] = site
+  end
+
+  opts.on("-r", "--source-env ENV", "Source environment to import from") do |env|
+    $options[:src_env] = env
+  end
+
+  opts.on("-t", "--target-env ENV", "Target environment to import to") do |env|
+    $options[:target_env] = env
   end
 
   opts.on("-l", "--limit COUNT", "Export/import only COUNT nodes. This may be helpful for testing when the node list is long") do |count|
     $options[:limit] = count.to_i
   end
 
-  opts.on("-s", "--source-nids NIDS", "List of source node nids separated by commas") do |nids|
+  opts.on("-n", "--source-nids NIDS", "List of source node nids separated by commas") do |nids|
     $options[:src_nids] = nids
   end
 
-  opts.on("-t", "--content-types TYPES", "Content types to work on") do |types|
+  opts.on("-c", "--content-types TYPES", "Content types to work on") do |types|
     $options[:content_types] = types
   end
 
-  opts.on("-n", "--languages LANGS", "Node languages - separated by commas, to work on") do |langs|
+  opts.on("-g", "--languages LANGS", "Node languages - separated by commas, to work on") do |langs|
     $options[:langs] = langs
   end
 
@@ -254,6 +361,14 @@ end
 
 opt_parser.parse!
 
-# Environment is required
-select_env if not $options[:env]
+select_site_name if not $options[:site_name]
+select_src_env if not $options[:src_env]
+select_target_env if not $options[:target_env]
+
+check_environments
+check_requirements
+
+prompt_source_nids if not $options[:src_nids]
+prompt_langs if not $options[:langs]
+
 restore_nodes
